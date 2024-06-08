30bde104c38d1177662485937c6170a4fa4d024a
Leanne <lemeyer.no@gmail.com>
2024-06-08 12:16:25 +0200

    upd.

--- JS/script.js
+++ JS/script.js
@@ -245,6 +245,7 @@ const hamburger = document.querySelector(".hamburger-icon");
 const navUl = document.querySelector(".nav-ul");
 const nav = document.querySelector("nav");
 const logo = document.querySelector(".My-logo");
+const header = document.querySelector("header");
 
 window.addEventListener("resize", displayHamburger);
 window.addEventListener("resize", closeNav);
@@ -255,7 +256,7 @@ function displayHamburger() {
   if (window.innerWidth > 750) {
     hamburger.style.display = "none";
     nav.style.display = "flex";
-    nav.style.gridArea = "1/2/3/2";
+    nav.style.gridArea = "1/3/3/3";
     nav.style.marginTop = "0";
     nav.style.marginLeft = "0";
     logo.style.gridArea = "1/1/1/1";
@@ -265,9 +266,10 @@ function displayHamburger() {
     hamburger.style.display = "flex";
     hamburger.style.gridArea = "1/1/2/2";
     nav.style.gridArea = "1/1/2/2";
-    nav.style.marginTop = "14%";
+    nav.style.marginTop = "3rem";
     nav.style.marginLeft = "-2rem";
     logo.style.gridArea = "1/2/2/3";
+    header.style.gridTemplateColumns = "1fr 1fr 1fr";
     logo.style.justifySelf = "center";
   } else {
     hamburger.style.display = "none";
@@ -278,6 +280,7 @@ function displayHamburger() {
 function openNav() {
   nav.style.display = "block";
   navUl.style.display = "block";
+  nav.style.position = "absolute";
 }
 
 function closeNav() {
@@ -287,6 +290,7 @@ function closeNav() {
   } else {
     nav.style.display = "flex";
     navUl.style.display = "flex";
+    nav.style.position = "relative";
   }
 }
 
